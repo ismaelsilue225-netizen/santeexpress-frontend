4a5c350ca1dc1b623b43c7bf8ff81a9aa059cbb5
ismaelsilue225-netizen <ismaelsilue225@gmail.com>
2026-06-11 11:52:38 +0000

    Update pharmaci.jsx

--- src/pharmaci.jsx
+++ src/pharmaci.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div style={{background:"#00A859",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column"}}>
       <h1 style={{color:"white",fontSize:36,fontWeight:900,margin:0}}>
-        SANTÉ<span style={{color:"#C8FFDF"}}>EXPRESS</span>
+        SANTÉ <span style={{color:"#C8FFDF"}}>EXPRESS</span>
       </h1>
       <p style={{color:"white",marginTop:16}}>
         Médicaments livrés chez vous
